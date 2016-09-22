a34755e7bae30a4e5db22f9f126197ba5b9355c0
WF | Gorjan Shukov <wf-gshukov@WFs-MacBook-Air.local>
2016-09-22 15:34:25 +0200

    added boolean variable for turning on and off caching

--- Demo/Manager/CacheManager.swift
+++ Demo/Manager/CacheManager.swift
@@ -11,16 +11,52 @@ import Foundation
 class CacheManager {
 	static var sharedInstance = CacheManager()
 	private let cache: NSCache
+	private var isCachingOn: Bool
 
 	init() {
 		cache = NSCache()
+		isCachingOn = true
 	}
 
-	func cachePosts(posts: [String: AnyObject], type: PostsType) {
-		cache.setObject(posts, forKey: type.rawValue)
+	func cachePosts(posts: [[String: AnyObject]], type: PostsType) {
+		if var cachedPosts = cache.objectForKey(type.rawValue) as? [[String: AnyObject]] {
+			cachedPosts.appendContentsOf(posts)
+			cache.setObject(cachedPosts, forKey: type.rawValue)
+		} else {
+			cache.setObject(posts, forKey: type.rawValue)
+		}
+
+		CoreDataManager.sharedInstance.savePosts(posts, type: type)
 	}
 
-	func getCachedPosts(type: PostsType) -> [String: AnyObject]? {
-		return cache.objectForKey(type.rawValue) as? [String: AnyObject]
+	func clearCachedPosts(type: PostsType) {
+		cache.removeObjectForKey(type.rawValue)
+		CoreDataManager.sharedInstance.removePosts(type)
+	}
+
+	func getCachedPosts(type: PostsType) -> [[String: AnyObject]]? {
+		if (!isCachingOn) {
+			return nil
+		} else if let posts = cache.objectForKey(type.rawValue) as? [[String: AnyObject]] {
+			return posts
+		} else {
+			return loadDiscCache(type)
+		}
+	}
+
+	func loadDiscCache(type: PostsType) -> [[String: AnyObject]]? {
+		if let savedCache = CoreDataManager.sharedInstance.getPosts(type) {
+			cache.setObject(savedCache, forKey: type.rawValue)
+			return savedCache
+		}
+		return nil
+	}
+
+	func setCachingON() {
+		isCachingOn = true
+	}
+
+	func setCachingOFF() {
+		isCachingOn = false
 	}
 }
