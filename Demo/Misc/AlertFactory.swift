0a881c99a69c1ad2ae7429ce9cde666624ae6313
WF | Gorjan Shukov <wf-gshukov@WFs-MacBook-Air.local>
2016-09-23 15:23:31 +0200

    moved all Alerts to AlertFactory

---
+++ Demo/Misc/AlertFactory.swift
@@ -0,0 +1,41 @@
+//
+//  AlertFactory.swift
+//  Demo
+//
+//  Created by WF | Gorjan Shukov on 9/23/16.
+//  Copyright © 2016 WF | Gorjan Shukov. All rights reserved.
+//
+
+import Foundation
+
+class AlertFactory {
+	static var retryActionClosure: Optional < () -> Void >
+	static var settingsActionClosure: Optional < () -> Void >
+
+	static func retryUpload() -> UIAlertController {
+
+		let alert = UIAlertController(title: "Upload failed", message: "Error image not uploaded. Would you like to retry?", preferredStyle: UIAlertControllerStyle.Alert)
+		let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+		let retryAction = UIAlertAction(title: "Retry", style: .Default) { (alert) in
+			retryActionClosure?()
+		}
+		alert.addAction(defaultAction)
+		alert.addAction(retryAction)
+
+		return alert
+	}
+
+	static func settings() -> UIAlertController {
+		let alert = UIAlertController(title: "No internet connection present", message: "Open settings?", preferredStyle: UIAlertControllerStyle.Alert)
+		let defaultAction = UIAlertAction(title: "Dismiss", style: .Default, handler: nil)
+		let openCellularAction = UIAlertAction(title: "Settings", style: .Default) { (alert) in
+			settingsActionClosure?()
+			UIApplication.sharedApplication().openURL(NSURL(string: "prefs:root=ROOT")!) // prefs:root=General&path=Music
+		}
+		alert.addAction(defaultAction)
+		alert.addAction(openCellularAction)
+
+		return alert
+	}
+
+}
